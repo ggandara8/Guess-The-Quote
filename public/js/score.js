3863872bc563e479ec863f9cf640d4fc9251931d
ggandara8 <ggandara8@gmail.com>
2020-07-15 22:06:08 -0500

    code running needs some css changes

---
+++ public/js/score.js
@@ -0,0 +1,30 @@
+$(document).ready(() => {
+  $.get("/api/user_data").then(data => {
+    $(".email").text(`User: ${data.email}`);
+  });
+  let user = JSON.parse(localStorage.getItem("user"));
+  let id = user.id;
+  console.log(user);
+  console.log(id);
+  $.get("/api/score/" + id).then(data => {
+    console.log(data);
+    let scoreid;
+    let arr = [];
+    for (i in data) {
+      scoreid = data[i].score; // all user scores
+      arr.push(data[i].score); // pushing user scores to arr 
+    }
+    console.log("Arr: " + arr);
+    let latestScore = scoreid; // users latest score
+    let maxScore = Math.max(...arr); // grabs user highest score from array
+    $(".finalScore").text(`Your score is: ${latestScore}pts`);
+    if (latestScore <= 10) {
+      $(".response").text("You can do better than that!");
+    } else {
+      $(".response").text("Good Job!");
+    }
+    $(".highScorebtn").on("click", () => {
+      $(".highScore").text(`Your best score is: ${maxScore}pts`);
+    });
+  });
+});
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,17 +1,14 @@
 // Requiring our models and passport as we've configured it
 const db = require("../models");
 const passport = require("../config/passport");
+const fetch = require("node-fetch");
 
 module.exports = function(app) {
   // Using the passport.authenticate middleware with our local strategy.
   // If the user has valid login credentials, send them to the members page.
   // Otherwise the user will be sent an error
   app.post("/api/login", passport.authenticate("local"), (req, res) => {
-    // Sending back a password, even a hashed password, isn't a good idea
-    res.json({
-      email: req.user.email,
-      id: req.user.id
-    });
+    res.json(req.user);
   });
 
   // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
@@ -42,12 +39,66 @@ module.exports = function(app) {
       // The user is not logged in, send back an empty object
       res.json({});
     } else {
-      // Otherwise send back the user's email and id
-      // Sending back a password, even a hashed password, isn't a good idea
       res.json({
         email: req.user.email,
         id: req.user.id
       });
     }
+  });
+  //posting score -----------------------------------------------------------------------------
+  app.post("/api/score", (req, res) => {
+    db.Score.create({
+      score: req.body.score,
+      UserId: req.body.userid
+    })
+      .then(newScore => {
+        res.json(newScore);
+      })
+      .catch(error => {
+        res.json(error);
+      });
+  });
+  //getting score ------------------------------------------------------------------------------
+  app.get("/api/score/:userid", (req, res) => {
+    db.Score.findAll({
+      where: {
+        UserId: req.params.userid
+      }
+    }).then(data => {
+      res.json(data);
+    });
+  });
+  //get id for high scores----------------------------------------------------
+  app.get("/api/score/:id", (req, res) => {
+    db.Score.findAll({
+      where: {
+        UserId: req.params.id
+      }
+    }).then(data => {
+      res.json(data);
+    });
+  });
+  //high score user id
+  app.get("/api/alluser_data/:id", (req, res) => {
+    db.User.findAll({
+      where: {
+        Id: req.params.id
+      }
+    }).then(dbUser => {
+      res.json(dbUser);
+    });
+  });
+  //all user scores
+  app.get("/api/score/", (req, res) => {
+    db.Score.findAll({}).then(dbScore => {
+      res.json(dbScore);
+    });
+  });
+  // gettign quotes and authors from the public quote garden api--------------------------------
+  app.get("/quotes", async (req, res) => {
+    const url = "https://quote-garden.herokuapp.com/api/v2/quotes/random";
+    const fetchRes = await fetch(url);
+    const json = await fetchRes.json();
+    res.json(json);
   });
 };
